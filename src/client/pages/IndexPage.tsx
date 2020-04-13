b54d8bd02290a9e8f6399906887b1ce12bbb9de9
J0rd1smit <jordismit20000@gmail.com>
2020-04-13 16:45:51 +0200

    leaf let setup

--- src/client/pages/IndexPage.tsx
+++ src/client/pages/IndexPage.tsx
@@ -1,103 +1,39 @@
-import React, {Fragment, useState, useEffect} from "react";
-import {OnClickEvent} from "../utils/ReactTypes";
+import React, {Fragment} from "react";
 import NavBar from "../components/NavBar";
-import {useRefState} from "../utils/ReactHelpers";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet"
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import {createGeoDataHook} from "../service/GeolocationService";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+            height: 500,
+            width: 500,
+        },
+    }),
+);
 
 function IndexPage(): JSX.Element {
-    const [value, setValue] = useState<string>("");
-    const [position, refPosition, setPosition] = useRefState<Coordinates|null>(null);
-    const [deviceOrientation, refDeviceOrientation, setDeviceOrientation] = useRefState<any>(undefined);
-    const [rotationDuringLastGeo, setRotationDuringLastGeo] = useState<number>(0);
+    const classes = useStyles();
+    const geoData = createGeoDataHook();
 
-
-
-    useEffect((): void => {
-        var options = {
-            enableHighAccuracy: true,
-            timeout: 20000,
-            maximumAge: 0,
-        };
-        const onSuccess = (pos: Position) => {
-            const coords = pos.coords;
-            if (refPosition.current?.heading !== coords.heading && refDeviceOrientation.current?.rotateDegrees !== undefined){
-                setRotationDuringLastGeo(refDeviceOrientation.current.rotateDegrees);
-            }
-            setPosition(coords);
-        };
-        const onError = (err: PositionError) => console.error(err.message);
-
-        navigator.geolocation.watchPosition(onSuccess, onError, options);
-
-        if (window.DeviceOrientationEvent){
-            window.addEventListener("compassneedscalibration", function(event) {
-                alert('Your compass needs calibrating! Wave your device in a figure-eight motion');
-                event.preventDefault();
-            }, true);
-            window.addEventListener("deviceorientationabsolute", function(event: any) {
-                setDeviceOrientation({
-                    rotateDegrees: event.alpha === null ? null: 360 - event.alpha,
-                    leftToRight: event.gamma,
-                    frontToBack: event.beta,
-                    absolute: event.absolute,
-                })
-            }, true);
-        }
-
-
-    }, []);
-
-
-
-
-    const onClick = async (e: OnClickEvent): Promise<void> => {
-        e.preventDefault();
-        const res = await fetch("/api/test");
-        const data = await res.json();
-        setValue(data["message"]);
-    }
-    const differance = () => {
-        if (deviceOrientation === undefined || position?.heading === null || position?.heading === undefined) {
-            return 0;
-        }
-        const currentView = deviceOrientation.rotateDegrees;
-        return  Math.abs(currentView - rotationDuringLastGeo) % 360
-    }
-
-    const currentHeading = () => {
-        if (deviceOrientation === undefined || position?.heading === null || position?.heading === undefined) {
-            return 0;
-        }
-        return (position.heading + differance()) % 360;
-    }
 
     // @ts-ignore
     return (
         <Fragment>
             <NavBar/>
-            <div>
-                <p>Message: {value}</p>
 
-                <button onClick={onClick}>test</button>
-            </div>
-            <div>
-                <p>accuracy: {position?.accuracy}</p>
-                <p>altitude: {position?.altitude}</p>
-                <p>altitudeAccuracy: {position?.altitudeAccuracy}</p>
-                <p>heading: {position?.heading}</p>
-                <p>latitude: {position?.latitude}</p>
-                <p>longitude: {position?.longitude}</p>
-            </div>
-           <div>
-                <p>rotateDegrees: {deviceOrientation?.rotateDegrees}</p>
-                <p>leftToRight: {deviceOrientation?.leftToRight}</p>
-                <p>frontToBack: {deviceOrientation?.frontToBack}</p>
-                <p>absolute: {deviceOrientation?.absolute? "True": "False"}</p>
-               <p>compassHeading: {deviceOrientation?.compassHeading}</p>
-                <p>rotationDuringLastGeo: {rotationDuringLastGeo}</p>
-
-                <p>differance: {differance()} </p>
-                <p>currentHeading: {currentHeading()}</p>
-
+            <div className={classes.root}>
+                <Map center={geoData.coord} zoom={18}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    />
+                    <Marker position={geoData.coord}>
+                        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                    </Marker>
+                </Map>
             </div>
 
         </Fragment>
