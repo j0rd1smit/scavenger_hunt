2acd8f05270f33b50570f890e50b62c7237cc69e
J0rd1smit <jordismit20000@gmail.com>
2020-04-20 21:29:57 +0200

    setup inline compas

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,7 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import React from "react";
 import ReactDOM from "react-dom";
-import IndexPage from "./pages/IndexPage";
+import IndexPage from "./pages/CanvasPage";
 
 
 const element = (
